9be08edbb4f6d46468d3be8ae88cf818626d2618
UV Panta <uvishere@gmail.com>
2024-09-30 15:43:07 +1000

    fix: issue with path

--- Install.ps1
+++ Install.ps1
@@ -9,6 +9,20 @@ function Install-PowerShell7AndWinget {
         return Get-Command winget -ErrorAction SilentlyContinue
     }
 
+    function Add-WingetToPath {
+        $wingetPath = [System.IO.Path]::Combine($env:USERPROFILE, "AppData\Local\Microsoft\WindowsApps")
+        $currentPath = [System.Environment]::GetEnvironmentVariable("Path", [System.EnvironmentVariableTarget]::Machine)
+
+        if ($currentPath -notlike "*$wingetPath*") {
+            Write-Output "Adding Winget path to system PATH..."
+            [System.Environment]::SetEnvironmentVariable("Path", "$currentPath;$wingetPath", [System.EnvironmentVariableTarget]::Machine)
+            Write-Output "Winget path added to system PATH. You may need to restart your session for changes to take effect."
+        }
+        else {
+            Write-Output "Winget path is already in the system PATH."
+        }
+    }
+
     Write-Output "Checking if Winget is installed..."
     $wingetInstalled = Test-WingetInstalled
 
@@ -25,6 +39,9 @@ function Install-PowerShell7AndWinget {
 
         Write-Output "Winget installation completed."
 
+        # Add Winget to PATH
+        Add-WingetToPath
+
         # Retry mechanism to ensure Winget is recognized
         $maxRetries = 5
         $retryCount = 0
@@ -38,8 +55,7 @@ function Install-PowerShell7AndWinget {
             Write-Error "Winget installation failed or Winget is not recognized after installation."
             return
         }
-    }
-    else {
+    } else {
         Write-Output "Winget is already installed."
     }
 
@@ -50,8 +66,7 @@ function Install-PowerShell7AndWinget {
         Write-Output "PowerShell 7 is not installed. Installing PowerShell 7 using Winget..."
         Start-Process winget -ArgumentList "install --id Microsoft.Powershell --source winget --accept-source-agreements --accept-package-agreements --silent" -Wait
         Write-Output "PowerShell 7 installation completed."
-    }
-    else {
+    } else {
         Write-Output "PowerShell 7 is already installed."
     }
 }
